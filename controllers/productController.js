56e1995818d5efb9e6104f5cd80e220e2a05de27
Gabriel Paez <gabpaez@gmail.com>
2024-03-05 10:55:02 -0300

    rutas de productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -63,12 +63,96 @@ export const createProduct = asyncError(async (req, res, next) => {
 //=================== Actualizar un Producto ==================//
 
 export const updateProduct = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler('Producto no encontrado', 404));
+  const { name, description, price, stock } = req.body;
+  if (name) product.name = name;
+  if (description) product.description = description;
+  if (price) product.price = price;
+  if (stock) product.stock = stock;
 
-  
-
+  await product.save();
 
   res.status(200).json({
     success: true,
     mensaje: 'El producto ha sido actualizado',
+  });
+});
+
+//====================== Agregar imagenes de productos ==============================//
+
+export const addProductImage = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler('Producto no encontrado', 404));
+
+  if (!req.file) return next(new ErrorHandler('Por favor ingrese una imagen'));
+
+  const file = getDataUri(req.file);
+  const myCloud = await cloudinary.uploader.upload(file.content);
+  const image = {
+    public_id: myCloud.public_id,
+    url: myCloud.secure_url,
+  };
+
+  product.images.push(image);
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: 'Imagen agregada exitosamente',
+  });
+});
+
+//====================== Borra imagenes de productos ==============================//
+
+export const deleteProductImage = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler('Producto no encontrado', 404));
+
+  const id = req.query.id;
+
+  if (!id)
+    return next(
+      new ErrorHandler('Por favor ingrese la identificacion de la imagen', 400)
+    );
+
+  let isExist = -1; //va a determinar la posicion de la imagen dentro del array si existe da la posicion, caso contrario -1
+
+  product.images.forEach((item, index) => {
+    if (item._id.toString() === id.toString()) isExist = index;
+  });
+
+  //console.log(isExist);
+
+  if (isExist < 0) return next(new ErrorHandler('La imagen no existe', 400));
+
+  await cloudinary.uploader.destroy(product.images[isExist].public_id);
+
+  product.images.splice(isExist, 1);
+
+  await product.save();
+
+  res.status(200).json({
+    success: true,
+    message: 'Imagen borrada exitosamente',
+  });
+});
+
+//====================== Borra producto ==============================//
+
+export const deleteProduct = asyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return next(new ErrorHandler('Producto no encontrado', 404));
+
+  //elimina todas imagenes que haya en el producto y luego eliminaos
+  for (let index = 0; index < product.images.length; index++) {
+    await cloudinary.uploader.destroy(product.images[index].public_id);
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: 'Producto borrado exitosamente',
   });
 });
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,7 +3,10 @@ import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.js';
 import { singleUpload } from '../middlewares/multer.js';
 import {
+  addProductImage,
   createProduct,
+  deleteProduct,
+  deleteProductImage,
   getAllProducts,
   getProducDetails,
   updateProduct,
@@ -14,8 +17,15 @@ const productRouter = express.Router();
 //get
 productRouter.get('/all', getAllProducts);
 //productRouter.get('/single/:id', getProducDetails);
-productRouter.route('/single/:id').get(getProducDetails).put(updateProduct);
 productRouter.post('/new', isAuthenticated, singleUpload, createProduct);
-
+productRouter
+  .route('/single/:id')
+  .get(getProducDetails)
+  .put(isAuthenticated, updateProduct)
+  .delete(isAuthenticated, deleteProduct);
+productRouter
+  .route('/images/:id')
+  .post(isAuthenticated, singleUpload, addProductImage)
+  .delete(isAuthenticated, deleteProductImage);
 
 export default productRouter;
